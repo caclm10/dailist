84154b3dc6d0fa6f7472f25ec3f12de251b4833a
caclm10 <lewinxander@gmail.com>
2025-06-12 07:30:19 +0700

    chore: finished setup

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { IndexView } from "@/views/index-view.tsx";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
 
-createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+import "@/assets/css/app.css";
+
+createRoot(document.getElementById("root")!).render(
+	<StrictMode>
+		<IndexView />
+	</StrictMode>,
+);
---
+++ src/views/index-view.tsx
@@ -0,0 +1,9 @@
+function IndexView() {
+	return (
+		<div className="text-destructive">
+			<h1>Hello World!</h1>
+		</div>
+	);
+}
+
+export { IndexView };
